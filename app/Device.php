2f5ce8ef6e9e5f1f363a1463df21316a87093fd8
Harry Brook-Dobson <harry.brook-dobson@agepartnership.com>
2020-07-17 11:05:00 +0100

    Create device model structure

--- app/Device.php
+++ app/Device.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Device extends Model
 {
-    //
+    protected $fillable = ['name', 'address', 'longitude', 'latitude', 'device_type', 'manufacturer', 'model', 'install_date', 'notes', 'eui', 'serial_number', 'upload_id'];
 }
--- database/migrations/2020_07_17_094417_create_devices_table.php
+++ database/migrations/2020_07_17_094417_create_devices_table.php
@@ -14,7 +14,19 @@ class CreateDevicesTable extends Migration
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('address');
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+            $table->string('device_type');
+            $table->string('manufacturer');
+            $table->string('model');
+            $table->dateTime('install_date');
+            $table->longText('notes');
+            $table->string('eui');
+            $table->string('serial_number');
+            $table->integer('upload_id');
             $table->timestamps();
         });
     }
