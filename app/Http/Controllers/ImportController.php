071f35b9a484b4d366d6cde001f346559f2e6b0b
Harry Brook-Dobson <harry.brook-dobson@agepartnership.com>
2020-07-17 16:19:15 +0100

    Change csv parsing to solve new line issue from google sheet export

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -28,19 +28,50 @@ class ImportController extends Controller
             'file' => 'required|mimes:csv,txt'
         ]);
         $path = request()->file('file')->getRealPath();
-        $file = file($path);
-        $data = array_slice($file, 1);
-
-        $parts = (array_chunk($data, 100));
+        $csvString = file_get_contents($path);
+        $data = $this->parse_csv($csvString);
+        $header = array_shift($data);
+        foreach ($data as $row) {
+            $csv[] = array_combine($header, $row);
+        }
+        $parts = (array_chunk($csv, 10));
         $i = 1;
         foreach($parts as $line) {
             $filename = base_path('resources/pendingDevices/'.date('y-m-d-H-i-s').$i.'.csv');
-            file_put_contents($filename, $line);
+            file_put_contents($filename, json_encode($line));
             $i++;
         }
 
         session()->flash('status', 'queued for importing');
 
         return redirect("import");
+    }
+
+    /**
+     * parse_csv handles csvs exported from google docs
+     */
+    private function parse_csv($csv_string, $delimiter = ",", $skip_empty_lines = true, $trim_fields = true)
+    {
+        $enc = preg_replace('/(?<!")""/', '!!Q!!', $csv_string);
+        $enc = preg_replace_callback(
+            '/"(.*?)"/s',
+            function ($field) {
+                return urlencode(utf8_encode($field[1]));
+            },
+            $enc
+        );
+        $lines = preg_split($skip_empty_lines ? ($trim_fields ? '/( *\R)+/s' : '/\R+/s') : '/\R/s', $enc);
+        return array_map(
+            function ($line) use ($delimiter, $trim_fields) {
+                $fields = $trim_fields ? array_map('trim', explode($delimiter, $line)) : explode($delimiter, $line);
+                return array_map(
+                    function ($field) {
+                        return str_replace('!!Q!!', '"', utf8_decode(urldecode($field)));
+                    },
+                    $fields
+                );
+            },
+            $lines
+        );
     }
 }
