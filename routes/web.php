445376a483fd6171128663eb476624bf892b7568
Harry Brook-Dobson <harry.brook-dobson@agepartnership.com>
2020-07-17 14:12:04 +0100

    Add routes for import csv page/form

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('import', 'ImportController@import');
+Route::post('import', 'ImportController@parseImport');
